4447571588de04179c44c22d2b7def1b24dc1cc8
Prateek Khandelwal <Prateek.Khandelwal@metacube>
2025-05-12 16:13:19 +0530

    changes

--- UI-7/script.js
+++ UI-7/script.js
@@ -1,24 +1,22 @@
-function showField(nextFieldId, currFieldId){
+const showField = (nextFieldId, currFieldId) => {
     const currField = document.getElementById(currFieldId);
     const nextField = document.getElementById(nextFieldId);
-    const input = currField.querySelector('input');
+    const input = currField?.querySelector('input');
 
-    if(input && input.value.trim() === ''){
+    if (input?.value.trim() === '') {
         alert('Please fill out this field before proceeding.');
         return;
     }
 
-    if (currFieldId === "nameField" && !validateName()) return;
-    if (currFieldId === "emailField" && !validateEmail()) return;
-    if (currFieldId === "passwordField" && !checkPassword()) return;
-    if (currFieldId === "cpasswordField" && !checkConfirmPassword()) return;
-    // if (currFieldId === "cpasswordField" && !checkContact()) return;
-    
-    if(currField){
-        currField.style.display = 'none';
+    if (
+        (currFieldId === "nameField" && !validateName()) ||
+        (currFieldId === "emailField" && !validateEmail()) ||
+        (currFieldId === "passwordField" && !checkPassword()) ||
+        (currFieldId === "cpasswordField" && !checkConfirmPassword())
+    ) {
+        return;
     }
 
-    if(nextField){
-        nextField.style.display = 'block';
-    }
-}
+    currField?.style.display = 'none';
+    nextField?.style.display = 'block';
+};
